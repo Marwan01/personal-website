ebcfd564e05ce31322ea39bdf5275db17f3af43e
Marouen Helali <marwanest@live.fr>
2021-11-12 08:53:45 -0800

    rearrange skills

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -8,6 +8,22 @@ function Skills() {
       <div className="container">
         <h1 className="title">Skills</h1>
         <div className="columns">
+        <div className="column is-6">
+            <div className="has-text-centered">
+              <span className="icon has-text-link">
+                <i className="fas fa-3x fa-laptop-code"></i>
+              </span>
+              <h2 className="title is-5">Dev</h2>
+            </div>
+            <SkillsList
+              skills={Resume.skills
+                .filter(skill => skill.keywords.includes("Dev"))
+                .reduce((obj, item) => {
+                  obj[item.name] = item.level;
+                  return obj;
+                }, {})}
+            />
+          </div>
           <div className="column is-6">
             <div className="has-text-centered">
               <span className="icon has-text-link">
@@ -18,22 +34,6 @@ function Skills() {
             <SkillsList
               skills={Resume.skills
                 .filter(skill => skill.keywords.includes("Ops"))
-                .reduce((obj, item) => {
-                  obj[item.name] = item.level;
-                  return obj;
-                }, {})}
-            />
-          </div>
-          <div className="column is-6">
-            <div className="has-text-centered">
-              <span className="icon has-text-link">
-                <i className="fas fa-3x fa-laptop-code"></i>
-              </span>
-              <h2 className="title is-5">Dev</h2>
-            </div>
-            <SkillsList
-              skills={Resume.skills
-                .filter(skill => skill.keywords.includes("Dev"))
                 .reduce((obj, item) => {
                   obj[item.name] = item.level;
                   return obj;
