2fe60ce177b73f9c4d1aa8804abba9a8ce7b6dd7
Marouen Helali <marwanest@live.fr>
2021-11-12 09:43:32 -0800

    change color to background color

--- src/components/elements/Badge.js
+++ src/components/elements/Badge.js
@@ -6,7 +6,7 @@ function Badge(props) {
     <div className="control">
       <div className="tags has-addons">
         <span className="tag is-dark">{props.text}</span>
-        <span className="tag has-text-link">
+        <span className="tag green">
           <i className={faIcon}></i>
         </span>
       </div>
